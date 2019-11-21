8106efa282b40fea931f2341b1d62fb4ec5d7f19
liaojinhua <liaojinhua60@gmail.com>
2019-11-21 08:34:29 +0800

    leet code 66 加1

--- src/com/lqhx/leetcode/Solution.java
+++ src/com/lqhx/leetcode/Solution.java
@@ -187,5 +187,48 @@ public class Solution {
 
     }
 
+    /**
+     * leet code 66 加1
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     * <p>
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     * <p>
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头
+     */
+    public int[] plusOne(int[] digits) {
+        if (digits == null) {
+            return null;
+        } else {
+            int mask = 0;
+            for (int i = digits.length - 1; i >= 0; i--) {
+                if (i == digits.length - 1) {
+                    digits[i]++;
+                    if (digits[i] >= 10) {
+                        mask = 1;
+                        digits[i] = 0;
+                    } else {
+                        mask = 0;
+                    }
+                } else {
+                    digits[i] += mask;
+                    if (digits[i] >= 10) {
+                        mask = 1;
+                        digits[i] = 0;
+
+                    } else {
+                        mask = 0;
+                    }
+                }
+            }
+            if (mask == 0) {
+                int result[] = new int[digits.length + 1];
+                result[0] = 1;
+                System.arraycopy(digits, 0, result, 1, digits.length);
+                return result;
+            }
+            return digits;
+        }
+
+    }
 }
 
