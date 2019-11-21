5bf2e6c4de818d3a19b47a831637544a86757bd1
liaojinhua <liaojinhua60@gmail.com>
2019-11-21 22:14:12 +0800

    上传Main

---
+++ src/com/lqhx/leetcode/Main.java
@@ -0,0 +1,24 @@
+package com.lqhx.leetcode;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+        Solution solution = new Solution();
+        solution.reverse(123);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
