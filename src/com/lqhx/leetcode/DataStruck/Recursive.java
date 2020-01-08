a03d86a2ac12be267cc0ae1012297e3f062f78ac
lqhx <liaojinhua60@gmail.com>
2020-01-08 21:37:41 +0800

    最大深度

--- src/com/lqhx/leetcode/DataStruck/Recursive.java
+++ src/com/lqhx/leetcode/DataStruck/Recursive.java
@@ -15,22 +15,19 @@ public class Recursive {
                 node = node.next;
             }
             return result;
-
         }
-
     }
 
-    public ListNode reverseRecursiveList(ListNode head)  {
+    public ListNode reverseRecursiveList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         } else {
             ListNode result = new ListNode(head.val);
             ListNode node = head.next;
-            result =reverseRecursiveList(node, result);
+            result = reverseRecursiveList(node, result);
             return result;
         }
     }
-
 
 
     public ListNode reverseRecursiveList(ListNode node, ListNode result) {
@@ -39,8 +36,53 @@ public class Recursive {
             nodeTemp.next = result;
             result = nodeTemp;
             node = node.next;
-            result = reverseRecursiveList(node,result);
+            result = reverseRecursiveList(node, result);
         }
         return result;
+    }
+
+    public int maxDepth(TreeNode root) {
+        int result = 0;
+        result = getDepth(root);
+        return result;
+    }
+
+    private int getDepth(TreeNode root) {
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int kthGrammar(int N, int K) {
+        StringBuffer[] results = new StringBuffer[N + 1];
+        for (int i = 0; i <= N; i++) {
+            if (i == 0) {
+                results[0] = new StringBuffer("0");
+            } else {
+                StringBuffer temp = results[i - 1];
+                StringBuffer temp2 = new StringBuffer("");
+                for (int j = 0; j < temp.length(); j++) {
+                    char tempChar = temp.charAt(j);
+                    if (tempChar == '0') {
+                        temp2.append("01");
+                    } else {
+                        temp2.append("10");
+                    }
+                }
+                results[i] = temp2;
+            }
+        }
+        if (K <= results[N].length()) {
+            return results[N].charAt(K) - '0';
+        } else {
+            return 0;
+        }
     }
 }
--- src/com/lqhx/leetcode/Main.java
+++ src/com/lqhx/leetcode/Main.java
@@ -5,12 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean isValid = solution.isValid("([)]");
         ListNode listNode1 = new ListNode(2);
 
         ListNode listNode3 = new ListNode(1);
 
         solution.mergeTwoLists(listNode1, listNode3);
-        System.out.println(isValid);
     }
 }
