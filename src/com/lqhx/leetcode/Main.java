9d2000a4152a979e08323bec8abbde3fb7768915
liaojinhua <liaojinhua60@gmail.com>
2020-02-06 10:36:56 +0800

    2020 0206 完成初级算法

--- src/com/lqhx/leetcode/Main.java
+++ src/com/lqhx/leetcode/Main.java
@@ -1,19 +1,20 @@
 package com.lqhx.leetcode;
 
 
-import com.lqhx.leetcode.DataStruck.ListNode;
+import com.lqhx.leetcode.DataStruck.MathSolution;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.toString());
-        int result =  solution.myAtoi("42");
-        System.out.println( solution.countAndSay(5));
-        ListNode temp1 = new ListNode(-129);
-        ListNode temp2 = new ListNode(-129);
-        temp1.next = temp2;
-        System.out.println(solution.isPalindrome(temp1));
-        System.out.println(solution.firstBadVersion(3));
+        MathSolution mathSolution = new MathSolution();
+        System.out.println(mathSolution.countPrimes(10));
+        System.out.println(mathSolution.isPowerOfThree(27));
+        System.out.println(mathSolution.hammingWeight(0B11111111111111111111111111111101));
+        System.out.println(mathSolution.hammingDistance(4,1));
+        System.out.println(mathSolution.isValid("()"));
     }
 }
